70a0fa839855d0fcf9e0c802a0cc0719c37f1252
samba8897 <sambasivarao3521@gmail.com>
2024-10-09 16:17:53 +0530

    updated newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://prolancer-backend.onrender.com/api/", 
   withCredentials: true,
 });
 
