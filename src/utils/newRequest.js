c81a842e1007118536a271621efa9c29a50a2284
samba8897 <sambasivarao3521@gmail.com>
2024-10-10 13:12:36 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,22 +1,27 @@
 import axios from "axios";
 
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const newRequest = axios.create({
-  baseURL: "https://prolancer-backend.onrender.com/api/",
+  baseURL: apiUrl, 
   withCredentials: true,
 });
 
-// Interceptor to add Authorization token to requests
-newRequest.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // Adjust based on where you're storing the token
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // Set the Authorization header
+newRequest.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); 
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; 
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default newRequest;
-
