589717d92c5284758b624a9be51b67d6a2a71f78
samba8897 <sambasivarao3521@gmail.com>
2024-10-10 16:38:35 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,26 @@
 import axios from "axios";
+import getCurrentUser from "./path/to/getCurrentUser"; // Adjust the path accordingly
 
 const newRequest = axios.create({
   baseURL: "https://prolancer-backend.onrender.com/api/",
   withCredentials: true,
 });
+
+
+newRequest.interceptors.request.use(
+  (config) => {
+    const currentUser = getCurrentUser(); 
+
+    // Check if the user and token exist
+    if (currentUser && currentUser.token) {
+      config.headers.Authorization = `Bearer ${currentUser.token}`; // Add token to request header
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default newRequest;
